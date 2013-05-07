de123821218e8d956bbe55f8bba3160a36b5df2d
LucasPadovan <zerobba8@hotmail.com>
2013-05-07 19:44:19 -0300

    mobile intervention y people

--- app/controllers/mobile_interventions_controller.rb
+++ app/controllers/mobile_interventions_controller.rb
@@ -29,7 +29,7 @@ class MobileInterventionsController < ApplicationController
   # GET /mobile_interventions/new.json
   def new
     @title = t('view.mobile_interventions.new_title')
-    @mobile_intervention = @intervention.build_mobile_intervention
+    @mobile_intervention = @endowment.build_mobile_intervention
 
     respond_to do |format|
       format.html # new.html.erb
@@ -47,12 +47,12 @@ class MobileInterventionsController < ApplicationController
   # POST /mobile_interventions.json
   def create
     @title = t('view.mobile_interventions.new_title')
-    @mobile_intervention = @intervention.build_mobile_intervention(params[:mobile_intervention])
+    @mobile_intervention = @endowment.build_mobile_intervention(params[:mobile_intervention])
 
     respond_to do |format|
       if @mobile_intervention.save
-        format.html { redirect_to [@intervention, @mobile_intervention], notice: t('view.mobile_interventions.correctly_created') }
-        format.json { render json: [@intervention, @mobile_intervention], status: :created, location: @mobile_intervention }
+        format.html { redirect_to [@intervention, @endowment,  @mobile_intervention], notice: t('view.mobile_interventions.correctly_created') }
+        format.json { render json: [@intervention, @endowment,  @mobile_intervention], status: :created, location: @mobile_intervention }
       else
         format.html { render action: 'new' }
         format.json { render json: @mobile_intervention.errors, status: :unprocessable_entity }
@@ -68,7 +68,7 @@ class MobileInterventionsController < ApplicationController
 
     respond_to do |format|
       if @mobile_intervention.update_attributes(params[:mobile_intervention])
-        format.html { redirect_to [@intervention, @mobile_intervention], notice: t('view.mobile_interventions.correctly_updated') }
+        format.html { redirect_to [@intervention, @endowment,  @mobile_intervention], notice: t('view.mobile_interventions.correctly_updated') }
         format.json { head :ok }
       else
         format.html { render action: 'edit' }
@@ -76,7 +76,7 @@ class MobileInterventionsController < ApplicationController
       end
     end
   rescue ActiveRecord::StaleObjectError
-    redirect_to edit_intervention_mobile_intervention(@intervention, @mobile_intervention), alert: t('view.mobile_interventions.stale_object_error')
+    redirect_to edit_intervention_endowement_mobile_intervention_path(@intervention, @endowment, @mobile_intervention), alert: t('view.mobile_interventions.stale_object_error')
   end
 
   # DELETE /mobile_interventions/1
@@ -94,5 +94,6 @@ class MobileInterventionsController < ApplicationController
   private
     def get_intervention
       @intervention = Intervention.find(params[:intervention_id])
+      @endowment = Endowment.find params[:endowement_id]
     end
 end
