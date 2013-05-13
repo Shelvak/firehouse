750e5fe5dc04b965304685d6bd18968ac4531c13
LucasPadovan <zerobba8@hotmail.com>
2013-05-12 22:12:53 -0300

    quick-buttons system

--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -1,3 +1,17 @@
 var extend_button_div = function(){
     $('#quick-button-div').css({ "height": $(window).height() });
+    $('#show-quick-buttons').hide();
+};
+
+var apply_quick_button_functions = function(){
+    $('#button-fire').click(function(){
+        $('#quick-button-div').slideUp(function(){
+            $('#show-quick-buttons').toggle('slow');
+        });
+    });
+
+    $('#show-quick-buttons').click(function(){
+        extend_button_div();
+        $('#quick-button-div').slideDown();
+    });
 };
