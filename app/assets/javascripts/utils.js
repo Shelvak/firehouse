61382c44913c1e6823acb3566d08d92dbd344a26
LucasPadovan <zerobba8@hotmail.com>
2013-05-16 15:47:51 -0300

    beta 1 sistema botones de rapida respuesta

--- app/assets/javascripts/utils.js
+++ app/assets/javascripts/utils.js
@@ -4,9 +4,24 @@ var extend_button_div = function(){
 };
 
 var apply_quick_button_functions = function(){
-    $('#button-fire').click(function(){
+    $('.alarm-button').click(function(){
+        console.log($(this).attr('target'));
+        switch( $(this).attr('target') ){
+            case 'fire':
+                $('#intervention_kind_e').prop('checked', true);
+                break;
+            case 'crash':
+                $('#intervention_kind_a').prop('checked', true);
+                break;
+            case 'rescue':
+                $('#intervention_kind_i').prop('checked', true);
+                break;
+            case 'other':
+                $('#intervention_kind_o').prop('checked', true);
+                break;
+        };
         $('#quick-button-div').slideUp(function(){
-            $('#show-quick-buttons').toggle('slow');
+            $('#show-quick-buttons').toggle();
         });
     });
 
