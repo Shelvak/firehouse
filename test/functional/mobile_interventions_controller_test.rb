ba6782efe4e1d7fa0f9ca74316c28342e1ad6e84
LucasPadovan <zerobba8@hotmail.com>
2013-10-29 00:53:14 -0300

    test funcionales mobile intervention

--- test/functional/mobile_interventions_controller_test.rb
+++ test/functional/mobile_interventions_controller_test.rb
@@ -4,61 +4,32 @@ class MobileInterventionsControllerTest < ActionController::TestCase
 
   setup do
     @mobile_intervention = Fabricate(:mobile_intervention)
+    @endowment = @mobile_intervention.endowment
+    @intervention = @endowment.intervention
     @user = Fabricate(:user)
     sign_in @user
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:mobile_interventions)
-    assert_select '#unexpected_error', false
-    assert_template "mobile_interventions/index"
-  end
-
-  test "should get new" do
-    get :new
+  test 'should show mobile_intervention' do
+    get :show, endowment_id: @endowment.to_param,
+        intervention_id: @intervention.to_param,
+        id: @mobile_intervention
     assert_response :success
     assert_not_nil assigns(:mobile_intervention)
+    assert_not_nil assigns(:endowment)
+    assert_not_nil assigns(:intervention)
     assert_select '#unexpected_error', false
-    assert_template "mobile_interventions/new"
+    assert_template 'mobile_interventions/show'
   end
 
-  test "should create mobile_intervention" do
-    assert_difference('MobileIntervention.count') do
-      post :create, mobile_intervention: Fabricate.attributes_for(:mobile_intervention)
-    end
-
-    assert_redirected_to mobile_intervention_url(assigns(:mobile_intervention))
-  end
-
-  test "should show mobile_intervention" do
-    get :show, id: @mobile_intervention
-    assert_response :success
-    assert_not_nil assigns(:mobile_intervention)
-    assert_select '#unexpected_error', false
-    assert_template "mobile_interventions/show"
-  end
-
-  test "should get edit" do
-    get :edit, id: @mobile_intervention
-    assert_response :success
-    assert_not_nil assigns(:mobile_intervention)
-    assert_select '#unexpected_error', false
-    assert_template "mobile_interventions/edit"
-  end
-
-  test "should update mobile_intervention" do
-    put :update, id: @mobile_intervention, 
-      mobile_intervention: Fabricate.attributes_for(:mobile_intervention, attr: 'value')
-    assert_redirected_to mobile_intervention_url(assigns(:mobile_intervention))
-  end
-
-  test "should destroy mobile_intervention" do
-    assert_difference('MobileIntervention.count', -1) do
-      delete :destroy, id: @mobile_intervention
-    end
-
-    assert_redirected_to mobile_interventions_path
+  test 'should update mobile_intervention' do
+    put :update, endowment_id: @endowment.to_param,
+        intervention_id: @intervention.to_param,
+        id: @mobile_intervention,
+        mobile_intervention: Fabricate.attributes_for(:mobile_intervention,
+                                                      observations: 'value')
+    assert_redirected_to intervention_endowment_mobile_intervention_url(
+                             assigns(:intervention),
+                             assigns(:endowment))
   end
 end
