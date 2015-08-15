3eba76acd013092811738d0ff3d537e5636c05df
Your Name <you@example.com>
2015-08-15 13:17:55 -0300

    Now

--- db/migrate/20150808205654_add_call_at_to_interventions.rb
+++ db/migrate/20150808205654_add_call_at_to_interventions.rb
@@ -1,5 +1,5 @@
 class AddCallAtToInterventions < ActiveRecord::Migration
   def change
-    add_column :interventions, :call_at, :datetime, null: false
+    add_column :interventions, :call_at, :datetime, null: false, default: 'NOW()'
   end
 end
