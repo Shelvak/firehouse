ea258d6e291f51cac1937441776238d078ea1f75
Néstor Coppi <nestorcoppi@gmail.com>
2016-01-01 20:43:51 -0300

    Little change

--- app/helpers/trucks_helper.rb
+++ app/helpers/trucks_helper.rb
@@ -1,9 +1,11 @@
 module TrucksHelper
   def fuel_type_selector(f)
-    f.input :fuel_type, collection: Truck::FUEL_TYPE, selected: Truck::FUEL_TYPE[0]
+    f.input :fuel_type, collection: Truck::FUEL_TYPE,
+      selected: f.objects.fuel_type || Truck::FUEL_TYPE.first
   end
 
   def truck_type_selector(f)
-    f.input :truck_type, collection: Truck::TRUCK_TYPES, selected: Truck::TRUCK_TYPES[0]
+    f.input :truck_type, collection: Truck::TRUCK_TYPES,
+      selected: f.objects.truck_type || Truck::TRUCK_TYPES.first
   end
 end
