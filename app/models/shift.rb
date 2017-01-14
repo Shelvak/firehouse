d0b500d941e87d7637086c3f6c1a2d3ebbde5ef4
Néstor Coppi <nestorcoppi@gmail.com>
2017-01-14 17:29:36 -0300

    Improve

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,7 +1,9 @@
 class Shift < ActiveRecord::Base
   KINDS = [
-    'voluntary',
-    'mandatory',
+    'volunteer_guard',
+    'mandatory_guard',
+    'internal_training',
+    'external_training',
     'others'
   ]
 
@@ -9,9 +11,17 @@ class Shift < ActiveRecord::Base
 
   belongs_to :firefighter
 
-  validates :start_at, :kind, presence: true
+  validates :firefighter, :start_at, :kind, presence: true
+  validates_datetime :start_at, allow_nil: true, allow_blank: true
+  validates_datetime :start_at, before: :finish,
+    allow_nil: true, allow_blank: true, if: :finish_at_present?
+  validates_datetime :finish_at, after: :start_at, allow_nil: true, allow_blank: true
 
   def to_s
     self.firefighter
+  end
+
+  def finish_at_present?
+    self.finish_at.present?
   end
 end
